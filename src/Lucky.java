ce3625ab77bf49d769d2cb6b48a72b1ed2baa26d
OAnsaharju <otto23002@student.hamk.fi>
2023-09-11 10:38:10 +0300

    Adding functionality to lucky 7

--- src/Lucky.java
+++ src/Lucky.java
@@ -9,23 +9,50 @@ public class Lucky {
 		Scanner sc = new Scanner(System.in);
 		Random random = new Random();
 		
+		
+		String creditsStr = "";
 		int min = 1;
 		int max = 9;
 		String win = "7";
 		
-		int n1 = random.nextInt(max + min) + min;
-		int n2 = random.nextInt(max + min) + min;
-		int n3 = random.nextInt(max + min) + min;
 		
-		String n1str = String.valueOf(n1);
-		String n2str = String.valueOf(n2);
-		String n3str = String.valueOf(n3);
+		System.out.println("Enter game credits: ");
 		
-		System.out.println(n1str + " " + n2str + " " + n3str);
+		creditsStr = sc.nextLine();
 		
-		if(n1str.equals(win) || n2str.equals(win) || n3str.equals(win) ) {
-			System.out.println("You Win!");
-		}
+		int credits = Integer.parseInt(creditsStr);
+		
+		do {
+			
+			
+			
+			int n1 = random.nextInt(max + min) + min;
+			int n2 = random.nextInt(max + min) + min;
+			int n3 = random.nextInt(max + min) + min;
+			
+			String n1str = String.valueOf(n1);
+			String n2str = String.valueOf(n2);
+			String n3str = String.valueOf(n3);
+		
+			
+			
+			System.out.println(n1str + " " + n2str + " " + n3str);
+			credits = credits -1;
+			
+			if (n1str.equals(win) || n2str.equals(win) || n3str.equals(win)){
+				System.out.println("You win");
+				credits = credits + 3;
+			}
+			
+			if (n1str.equals(win) && n2str.equals(win) || n1str.equals(win) && n3str.equals(win) || n2str.equals(win) && n3str.equals(win) ) {
+				System.out.println("Nice you got two sevens!");
+				credits = credits + 5;
+			}
+		   
+		   
+		   
+			}
+		while (credits > 0);
 		
 	}
 
